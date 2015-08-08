e2cbbce2320bab16a8567dfa759d8f8ab38d3285
Michael Matuzak <mmatuzak@gmail.com>
2015-08-08 15:14:11 -0700

    adding the correct sprites and bg and hooking up bullets

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-// canvas is 2X the GB pixels
-
-var game = new Phaser.Game(320, 288, Phaser.AUTO, 'Super Space Prime', { 
+var game = new Phaser.Game(160, 144, Phaser.AUTO, 'Super Space Prime', { 
   preload: preload,
   create: create,
   update: update
@@ -9,14 +7,36 @@ var game = new Phaser.Game(320, 288, Phaser.AUTO, 'Super Space Prime', {
 var player
 var cursors
 var fireButton
+var bullet
+var bullets
+var bulletTime = 0
 
 function preload() {
-  game.load.image('ship', 'triangle.png')
+  game.load.image('bg', 'bg.png')
+  game.load.image('bullet', 'bullet.png')
+  game.load.image('ship', 'ship.png')
 }
 
 function create() {
+  game.renderer.clearBeforeRender = false
+  game.renderer.roundPixels = true
+
+  game.renderer.clearBeforeRender = false
+  game.renderer.roundPixels = true
+
   game.physics.startSystem(Phaser.Physics.ARCADE)
-  player = game.add.sprite(100, 100, 'ship')
+
+  game.add.tileSprite(0, 0, game.width, game.height, 'bg')
+
+  bullets = game.add.group()
+  bullets.enableBody = true
+  bullets.physicsBodyType = Phaser.Physics.ARCADE
+  bullets.createMultiple(40, 'bullet')
+  bullets.setAll('anchor.x', 0.5)
+  bullets.setAll('anchor.y', 0.5)
+
+
+  player = game.add.sprite(75, 75, 'ship')
   player.anchor.set(0.5, 0.5)
 
   game.physics.enable(player, Phaser.Physics.ARCADE)
@@ -45,8 +65,17 @@ function update() {
   }
 
   if (fireButton.isDown) {
-    console.log('fire')
-    // firebullet()
+    if (game.time.now > bulletTime) {
+      bullet = bullets.getFirstExists(false)
+
+      if (bullet) {
+        bullet.reset(player.body.x + 10, player.body.y + 10)
+        bullet.lifespan = 1000
+        bullet.rotation = player.rotation
+        game.physics.arcade.velocityFromRotation(player.rotation, 400, bullet.body.velocity)
+        bulletTime = game.time.now + 200
+      }
+    }
   }
 
   // collision stuff
