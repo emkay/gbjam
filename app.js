ba3b42961ee4e0914fa27b1b057e6aeeca5901a4
Michael Matuzak <mmatuzak@gmail.com>
2015-08-14 19:31:01 -0700

    game menus and game over menu

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var boot = require('./lib/boot')
-//var preloader = require('./lib/preloader')
-//var mainMenu = require('./lib/main-menu')
-var gameLoop = require('./lib/game')
+var mainMenu = require('./lib/main-menu')
+var gameLoop = require('./lib/game-loop')
 
 var screenX = 160
 var screenY = 144
@@ -9,8 +8,7 @@ var screenY = 144
 var game = new Phaser.Game(screenX, screenY, Phaser.CANVAS)
 
 game.state.add('boot', boot)
-//game.state.add('preloader', preloader)
-//game.state.add('mainMenu', mainMenu)
+game.state.add('mainMenu', mainMenu)
 game.state.add('gameLoop', gameLoop)
 
 game.state.start('boot')
